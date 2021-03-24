7769595a965d25cbf5d83492fa48d2cd3a8d71bb
agungtsmn <0812agung@gmail.com>
2021-03-24 11:40:01 +0700

    Class Manusia

---
+++ evaluasi_dasar_pbo/lib/manusia.dart
@@ -0,0 +1,10 @@
+// Amanda Agung Permata
+// 6304191217
+// Pengembangan Aplikasi Mobile Multiplatform
+
+// ------------------------------------------
+// Class Manusia
+
+class Manusia {
+  String nama;
+}
